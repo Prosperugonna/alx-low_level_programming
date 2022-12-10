9cb04024d24cf7a4b60f4f2a9681970ded1be45d
prosperugonna <prosperugonna@gmail.com>
2022-12-10 06:20:54 -0800

    Task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,20 +6,31 @@
  */
 int main(void)
 {
+	int num = 0;
 	int num;
 
+	while (1)
 	for (num = 0; num <= 9; num++)
 	{
 		putchar((num % 10) + '0');
 		if (num == 9)
+		{
+			break;
+		}
+		else
+		{
+			putchar(44);
+			putchar(32);
+		}
+		num++;
 			continue;
 
-		putchar(',');
+		putchar(',')
 		putchar(' ');
+
 	}
 
 	putchar('\n');
 
 	return (0);
 }
-
