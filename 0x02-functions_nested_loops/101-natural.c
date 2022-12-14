a006c822332e8fa787b3c9d041a57d0f701f462e
prosperugonna <prosperugonna@gmail.com>
2022-12-14 02:43:07 -0800

    Task 13

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -7,15 +7,19 @@
  */
 int main(void)
 {
-	int i;
-	int sum = 0;
+	int i = 1;
+	int total = 0;
 
-	for (i = 3; i < 1024; ++i)
+	while (i < 1024)
 	{
-		if (i % 3 == 0 || i % 5 == 0)
-			sum += i;
+		if (i % 3 == 0)
+			total += i;
+		else if (i % 5 == 0)
+			total += i;
+
+		i++;
 	}
-	printf("%d\n", sum);
+	printf("%d\n", total);
 
 	return (0);
 }
