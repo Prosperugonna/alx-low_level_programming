8cfbfac3d1ce55c6cb3ea6e2d200f78de9cd7454
prosperugonna <prosperugonna@gmail.com>
2023-01-09 02:06:27 -0800

    task 0

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,12 +1,8 @@
 #include "main.h"
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
+#include <stdio.h>
 int main(void)
 {
-	_puts("\"At the end of the day, i am a hacker\"\n\t- Kevin Mitnick");
-	return (0);
+_puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
+
+return (0);
 }
