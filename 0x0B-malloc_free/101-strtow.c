e63af030670107133f4410492a3b3905b8535adf
prosperugonna <prosperugonna@gmail.com>
2023-01-09 09:34:55 -0800

    string into words

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -7,10 +7,10 @@ int count_words(char *str);
 char **strtow(char *str);
 
 /**
- * strtow - concatenates all the arguments of your program
- * @str: string
- * @av: arguments
- * Return: a pointer to a new string
+ * word_len - Locates the index marking the end of the
+ * first word contained within a string.
+ * @str: The string to be searched.
+ * Return: The index marking the end of the initial word pointed to by str.
  */
 int word_len(char *str)
 {
