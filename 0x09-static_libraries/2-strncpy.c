9b1a652f4fbdf32b8f9aba358f3e7f6670003cfd
prosperugonna <prosperugonna@gmail.com>
2023-01-10 10:51:53 -0800

    task 0

--- 0x09-static_libraries/2-strncpy.c
+++ 0x09-static_libraries/2-strncpy.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * _strncpy - copy a string
  * @dest: input value
@@ -8,7 +7,6 @@
  *
  * Return: dest
  */
-
 char *_strncpy(char *dest, char *src, int n)
 {
 	int j;
@@ -24,6 +22,5 @@ char *_strncpy(char *dest, char *src, int n)
 		dest[j] = '\0';
 		j++;
 	}
-	
 	return (dest);
 }
