356b28e8b1d9d087f187e022fc83ba35b23748c0
Prosperugonna <prosperugonna@gmail.com>
2023-01-26 04:01:55 -0800

    task 4

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,54 +1,16 @@
-/*
- * File: 3-add_node_end.c
- * Auth: Brennan D Baraban
- */
-
 #include "lists.h"
-#include <string.h>
 
 /**
- * add_node_end - Adds a new node at the end
- *                of a list_t list.
- * @head: A pointer the head of the list_t list.
- * @str: The string to be added to the list_t list.
- *
- * Return: If the function fails - NULL.
- *         Otherwise - the address of the new element.
+ *free_list - Write a function that frees a list_t list.
+ *@head: pointer pointed to the start of the list .
+ *Return: void
  */
-list_t *add_node_end(list_t **head, const char *str)
+void free_list(list_t *head)
 {
-char *dup;
-int len;
-list_t *new, *last;
-
-new = malloc(sizeof(list_t));
-if (new == NULL)
-return (NULL);
-
-dup = strdup(str);
-if (str == NULL)
+while (head)
 {
-free(new);
-return (NULL);
+free(head->str);
+free(head);
+head = head->next;
 }
-
-for (len = 0; str[len];)
-len++;
-
-new->str = dup;
-new->len = len;
-new->next = NULL;
-
-if (*head == NULL)
-*head = new;
-
-else
-{
-last = *head;
-while (last->next != NULL)
-last = last->next;
-last->next = new;
-}
-
-return (*head);
 }
