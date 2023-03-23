f7f34fc2a3a324baa1ef4c3ee93b4aec03ac67ab
Prosperugonna <prosperugonna@gmail.com>
2023-03-23 03:13:25 -0700

    fix

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -q -O /tmp/cheatme.so https://github.com/prosperugonna/low_level_programming/raw/master/0x18-dynamic_libraries/cheatme.so
-export LD_PRELOAD=/tmp/cheatme.so
+wget -P .. https://raw.githubusercontent.com/Prosperugonna/low_level_programming/master/0x18-dynamic_libraries/libgiga.so
+export LD_PRELOAD="$PWD/../libgiga.so"
