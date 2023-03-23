d8e642da26db0fa69be3a5f78407b7f316abd728
Prosperugonna <prosperugonna@gmail.com>
2023-03-23 12:18:08 -0700

    fix

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/Prosperugonna/alx-low_level_programming/raw/master/0x18-dynamic_libraries/injectcode.so
-export LD_PRELOAD=/tmp/injectcode.so
+wget -P /tmp/ https://raw.github.com/Prosperugonna/alx-low_level_programming/master/0x18-dynamic_libraries/nrandom.so
+export LD_PRELOAD=/tmp/nrandom.so
