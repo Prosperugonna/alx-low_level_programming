2ed2f42c330c89ea116d2636ba1788dce6c8ed83
Prosperugonna <prosperugonna@gmail.com>
2023-04-07 06:50:08 -0700

    fix

--- 0x1A-hash_tables/tests/0-main.c
+++ 0x1A-hash_tables/tests/0-main.c
@@ -1,13 +1,8 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
-#include "../hash_tables.h"
+#include "hash_tables.h"
 
-/**
- * main - check the code for Holberton School students.
- *
- * Return: Always EXIT_SUCCESS.
- */
 int main(void)
 {
 	hash_table_t *ht;
